85db7b9510b059de9f7634a283e8fed698a45723
unknown <hoangdinh37k02@gmail.com>
2022-02-18 20:56:58 +0700

    systemtime

---
+++ src/ss1_introduce/practice/SystemTime.java
@@ -0,0 +1,11 @@
+package ss1_introduce.practice;
+
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+
+        System.out.println("Now is: " + now);
+    }
+}
